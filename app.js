0e1ab5c8a6be8f3a13ccdb005d9c23a1f9572b83
TahiaBushra <bushratahia@gmail.com>
2023-12-12 20:51:35 +0600

    Slice & Splice Done

---
+++ app.js
@@ -0,0 +1,34 @@
+// //  Simple Array Methods
+
+// const numbers = [12, 14, 26, 85, 57, 95, 3, 17];
+
+// // unshift() : 41 will be added first
+// numbers.unshift(41);
+// console.log(numbers);
+// //shift() : 41 will be removed from first
+// numbers.shift(41);
+// console.log(numbers);
+
+// // push() : 5 Will be added last
+// numbers.push(5);
+// console.log(numbers);
+// // push() : 5 Will be removed from last
+// numbers.pop(5);
+// console.log(numbers);
+
+// // at() : Will show the array number by putting the index number
+// console.log(numbers.at(2));
+// // indexOf () : Will show the index number of array number
+// console.log(numbers.indexOf(95));
+// // includes() : Will show a bulean result (true or false) either the number is in the array or not.
+// console.log(numbers.includes(55));
+
+// const arr = [2, 15, 86, 75, 21, 35, 34, 48];
+
+// // slice(): Will cut the numbers from array acording to index number.
+// const sliceArr = arr.slice(0, 5);
+// console.log(sliceArr);
+
+// // splice(): Will cut the numbers from array acording to index number & count number also deletes the number from main array.
+// const spliceArr = arr.splice(0, 7);
+// console.log(spliceArr);
