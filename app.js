ef3c52565284f0d09f5e1b87ed49d8cf4a24d6ca
TahiaBushra <bushratahia@gmail.com>
2023-12-12 21:57:01 +0600

    Sort , Flat & Flat Map Done

--- app.js
+++ app.js
@@ -77,11 +77,36 @@
 // const newNum = newArr.map((num) => num * 2).filter((num) => num > 10).forEach((num) => (sum += num));
 // console.log(sum);
 
-// Reduce
-const myNumbers = [10, 20, 30, 40, 50];
-const sum = myNumbers.reduce((sum, num) => (sum += num));
-console.log(sum);
+// // Reduce
+// const myNumbers = [10, 20, 30, 40, 50];
+// const sum = myNumbers.reduce((sum, num) => (sum += num));
+// console.log(sum);
 
-// Reduce Right
-const product = myNumbers.reduceRight((product, num) => (product *= num));
-console.log(product);
+// // Reduce Right
+// const product = myNumbers.reduceRight((product, num) => (product *= num));
+// console.log(product);
+
+// sort()
+
+// Example-1
+const foods = ["Custard Apple", "Guava", "Choclate", "Pomgranet", "Ice-Cream", "Cake"];
+foods.sort();
+console.log(foods);
+
+// Example-2
+const shoppingCart = [
+    { id: 1, title: "Shirt", price: 1000, rating: 4.5 },
+    { id: 2, title: "T-Shirt", price: 950, rating: 5 },
+    { id: 3, title: "Sari", price: 2000, rating: 4.9 },
+    { id: 4, title: "Kamiz", price: 3000, rating: 4.2 },
+    { id: 5, title: "Jeans", price: 1500, rating: 4.7 },
+];
+const sortedCart = shoppingCart.sort((a, b) => a.price - b.price);
+const sortedCart2 = shoppingCart.sort((a, b) => a.title.localeCompare(b.title));
+console.log(sortedCart);
+console.log(sortedCart2);
+
+// flat() & flatMap()
+const nestedArray = [1, 2, [4, 5], 7, 8];
+const flattedArray = nestedArray.flat(2).flatMap((el) => el + 4);
+console.log(flattedArray);
